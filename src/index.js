f1cd54a06e790db9c12f977d78f8196071cc8446
Grzafnan <Grzafnan>
2022-11-24 00:22:27 +0600

    added navbar

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import AuthProvider from './contexts/AuthProvider/AuthProvider';
+import { Toaster } from 'react-hot-toast';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <Toaster position='top-center' />
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
